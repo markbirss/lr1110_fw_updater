0070342ddec78e90fda0ee23f1ba71182e760c77
Mark Trevor Birss <markbirss@gmail.com>
2024-09-12 12:25:06 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,49 @@
-/*********************************************************************
- This is an example for our nRF52 based Bluefruit LE modules
-
- Pick one up today in the adafruit shop!
-
- Adafruit invests time and resources providing this open source code,
- please support Adafruit and open-source hardware by purchasing
- products from Adafruit!
-
- MIT license, check LICENSE for more information
- All text above, and the splash screen below must be included in
- any redistribution
-*********************************************************************/
-//#include <Adafruit_LittleFS.h>
 #include <InternalFileSystem.h>
 #include <Adafruit_TinyUSB.h> // for Serial
+#include <RadioLib.h>
 
-//using namespace Adafruit_LittleFS_Namespace;
+#define RADIOLIB_LR1110_FIRMWARE_0401
+// include the firmware image
+#include <modules/LR11x0/LR11x0_firmware.h>
+
+LR1110 radio = new Module((32 + 13), (32 + 12), (32 + 11), (32 + 10));
 
 void enterDfuMode()
 {
     enterUf2Dfu();
+}
+
+void printVersions() {
+  LR11x0VersionInfo_t version;
+  Serial.print(F("[LR1110] Reading firmware versions ... "));
+  int16_t state = radio.getVersionInfo(&version);
+  if (state == RADIOLIB_ERR_NONE) {
+    Serial.println(F("success!"));
+
+    Serial.print(F("[LR1110] Device: "));
+    Serial.println(version.device);
+
+    Serial.print(F("[LR1110] Base firmware: "));
+    Serial.print(version.fwMajor);
+    Serial.print('.');
+    Serial.println(version.fwMinor);
+
+    Serial.print(F("[LR1110] WiFi firmware: "));
+    Serial.print(version.fwMajorWiFi);
+    Serial.print('.');
+    Serial.println(version.fwMinorWiFi);
+
+    Serial.print(F("[LR1110] GNSS firmware: "));
+    Serial.print(version.fwGNSS);
+    Serial.print('.');
+    Serial.println(version.almanacGNSS);
+
+  } else {
+    Serial.print(F("failed, code "));
+    Serial.println(state);
+    while (true) { delay(10); }
+  
+  }
 }
 
 // the setup function runs once when you press reset or power the board
@@ -27,20 +51,47 @@ void setup()
 {
   Serial.begin(115200);
   while ( !Serial ) delay(10);   // for nrf52840 with native usb
-  Serial.println("Meshtastic nRF52 Factory Erase firmware for the Meshtastic project.");
+  Serial.println("ME25LS01-4Y10TD LR1110 Modem firmware updater to Base: 4.1 WiFi: 3.2 GNSS: 2.0");
   Serial.println();
 
-  // Initialize Internal File System
-  InternalFS.begin();
+  // initialize LR1110 with default settings
+  Serial.print(F("[LR1110] Initializing ... "));
+  int state = radio.begin();
+  if (state == RADIOLIB_ERR_NONE) {
+    Serial.println(F("success!"));
+  } else {
+    Serial.print(F("failed, code "));
+    Serial.println(state);
+    while (true) { delay(10); }
+  }
 
-  Serial.print("Formating ... ");
+  // print the firmware versions before the update
+  printVersions();
+
+  // prompt the user
+  Serial.println(F("[LR1110] Send any character to start the update"));
+  while(!Serial.available()) { delay(1); }
+
+  // upload update into LR11x0 non-volatile memory
+  Serial.print(F("[LR1110] Updating firmware, this may take several seconds ... "));
+  state = radio.updateFirmware(lr11xx_firmware_image, RADIOLIB_LR11X0_FIRMWARE_IMAGE_SIZE);
+  /*
+    use the following if you enabled RADIOLIB_LR1110_FIRMWARE_IN_RAM
+    state = radio.updateFirmware(lr11xx_firmware_image, RADIOLIB_LR11X0_FIRMWARE_IMAGE_SIZE, false);
+  */
+  if (state == RADIOLIB_ERR_NONE) {
+    Serial.println(F("success!"));
+  } else {
+    Serial.print(F("failed, code "));
+    Serial.println(state);
+    while (true) { delay(10); }
+  }
+
+  // print the firmware versions after the update
+  printVersions();
+
   delay(1); // for message appear on monitor
 
-  // Format
-  InternalFS.format();
-
-  Serial.println("Done, rebooting device into DFU mode");
-  delay(1); // for message appear on monitor
   enterDfuMode();
 }
 
